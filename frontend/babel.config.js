845f9d2b0a914c0f51df8e54965671f926ba524d
Victor dos Santos <vy.santos@live.com>
2020-04-30 19:07:26 -0300

    add react ambient

---
+++ frontend/babel.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  presets: [
+    //  ele converte apenas as funcionalidades mais recente que o browser não entende. 
+    //  Ele entende o ambiente e converte o código baseado nisso.
+    '@babel/preset-env',
+
+    // ele add as funcionalidades do react dentro do projeto. 
+    '@babel/preset-react'
+  ],
+}
---
+++ frontend/webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path')
+
+module.exports = {
+  //arquivo de entrada vai
+  entry: path.resolve(__dirname, 'src', 'index.js'),
+
+  //qual arquivo vai ser gerado depois de convertido
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js'
+  },
+
+  // serviço para rodar igual o nodemon
+  devServer: {
+    contentBase: path.resolve(__dirname, 'public'),
+  },
+
+  //regras
+
+  module: {
+    rules: [
+      //cada objeto é um loader, como expressão regular.
+      // instrução: Toda vez que tive um arquivo na minha aplicação que não estiver em node_modules
+      //converte ele pra mim usando o babel-loader
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        }
+      }
+    ]
+  }
+
+}
