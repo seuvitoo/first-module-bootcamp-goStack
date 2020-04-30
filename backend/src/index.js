c2d026c852631ddf50f6eacddcdf80e9abc96651
Victor dos Santos <vy.santos@live.com>
2020-04-30 15:40:48 -0300

    add middlewares

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,12 +1,28 @@
 const express = require('express');
-const { uuid } = require('uuidv4')
+const { uuid, isUuid } = require('uuidv4')
 
 const app = express();
-
 app.use(express.json());
 
-
 const projects = [];
+
+function logRequests(request, response, next) {
+  const { method, url } = request;
+  const logLabel = `[${method.toUpperCase()}] ${url}`;
+
+  next();
+  console.timeEnd(logLabel)
+}
+
+function validateProjectId(request, response, next) {
+  const { id } = request.params;
+  if (!isUuid(id)) return response.status(400).json({ error: 'Invalid project ID' })
+
+  return next();
+}
+
+app.use(logRequests);
+app.use('/projects/:id', validateProjectId)
 
 app.get('/projects', (request, response) => {
   const { title } = request.query;
@@ -29,7 +45,7 @@ app.post('/projects', (request, response) => {
   return response.json(project);
 });
 
-app.put('/projects/:id', (request, response) => {
+app.put('/projects/:id', validateProjectId, (request, response) => {
   const { id } = request.params;
   const { title, owner } = request.body;
 
@@ -47,7 +63,7 @@ app.put('/projects/:id', (request, response) => {
   return response.json(project);
 });
 
-app.delete('/projects/:id', (request, response) => {
+app.delete('/projects/:id', validateProjectId, (request, response) => {
   const { id } = request.params;
 
   const projectIndex = projects.findIndex(project => project.id === id);
