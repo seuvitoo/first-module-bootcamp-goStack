39d33a0b220788f671b4ada3684a867934135eed
Victor dos Santos <vy.santos@live.com>
2020-04-30 01:01:00 -0300

    create basic routes

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,34 +1,64 @@
 const express = require('express');
+const { uuid } = require('uuidv4')
 
 const app = express();
 
-app.get('/', (request, response) => {
-  return response.json({ message: 'Hello World' });
+app.use(express.json());
+
+
+const projects = [];
+
+app.get('/projects', (request, response) => {
+  const { title } = request.query;
+
+  const results = title
+    ? projects.filter(project => project.title.includes(title))
+    : projects
+
+  return response.json(projects);
 });
 
 app.post('/projects', (request, response) => {
-  return response.json([
-    'Projeto 1',
-    'Projeto 3',
-    'Projeto 2',
-  ]);
+  const { title, owner } = request.body;
+  console.log(title)
+
+  const project = { id: uuid(), title, owner }
+
+  projects.push(project)
+
+  return response.json(project);
 });
 
 app.put('/projects/:id', (request, response) => {
-  return response.json([
-    'Projeto 1',
-    'Projeto 3',
-    'Projeto 2',
-  ]);
+  const { id } = request.params;
+  const { title, owner } = request.body;
+
+  const projectIndex = projects.findIndex(project => project.id === id);
+
+  if (projectIndex < 0) return response.status(400).json({ error: 'Project Not Found' })
+
+  const project = {
+    id,
+    title,
+    owner
+  }
+  projects[projectIndex] = project
+
+  return response.json(project);
 });
 
 app.delete('/projects/:id', (request, response) => {
-  return response.json([
-    'Projeto 1',
-    
-  ]);
+  const { id } = request.params;
+
+  const projectIndex = projects.findIndex(project => project.id === id);
+
+  if (projectIndex < 0) return response.status(400).json({ error: 'Project Not Found' })
+
+  projects.splice(projectIndex, 1)
+
+  return response.status(204).send();
 });
 
-app.listen(3333, ()=>{
+app.listen(3333, () => {
   console.log('🚀Server started🚀')
 });
